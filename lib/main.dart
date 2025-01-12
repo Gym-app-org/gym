6577dce284e82ba85e404da4491515f8f4956ac5
rustic_flutter <rustic_flutter@bwg-io.site>
2025-01-12 22:10:12 +0300

    chore: add project arch

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
-
-import 'src/features/app/app.dart';
+import 'main_development.dart' as dev;
 
 void main() {
-  runApp(
-    const GymApp(),
-  );
+  dev.main();
+}
+
+class GymApp extends StatefulWidget {
+  const GymApp({super.key});
+
+  @override
+  State<GymApp> createState() => _GymAppState();
+}
+
+class _GymAppState extends State<GymApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router();
+  }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:gym_app/main.dart';
-import 'package:gym_app/src/features/app/app.dart';
+import 'package:gym_app/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
